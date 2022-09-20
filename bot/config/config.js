8b495ca8992380e245a89ae1f19f28968f68f008
venusgalstar <venusgalstar@gmail.com>
2022-09-21 03:45:18 +0900

    update bot

--- bot/config/config.js
+++ bot/config/config.js
@@ -5,7 +5,7 @@ var config = {
     "HOST"      : "127.0.0.1",
     "USER"      : "root",
     "PASSWORD"  : "",
-    "DATABASE"  : "walletdb",
+    "DATABASE"  : "tothesmartbot",
     "PORT"      : 9000,
     "OPKEY"     : "644f579152f99037f56150bcac0420b69936dbf5b29b3267cbe9ce21601c3060",
     "NET_ID"    : "4",
